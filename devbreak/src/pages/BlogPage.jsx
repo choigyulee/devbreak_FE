faabc04eef9ae3b959db29dc6c3eb3bce0497f85
norangsuji <sujihong93@gmail.com>
2024-11-10 02:08:05 +0900

    feat <routes, Blog/Contents Page> 라우트 수정에 따른 하위페이지 수정

--- devbreak/src/pages/BlogPage.jsx
+++ devbreak/src/pages/BlogPage.jsx
@@ -1,11 +1,15 @@
+import { useParams } from "react-router-dom";
 import styled from "@emotion/styled";
 function BlogPage() {
+  const { blogId } = useParams(); // URL에서 articleId 가져오기
   return (
     <>
-      <Container>BlogPage</Container>
+      <Container>Contents for Article ID: {blogId}</Container>
     </>
   );
 }
 export default BlogPage;
 
-const Container = styled.div``;
+const Container = styled.div`
+  color: white;
+`;
