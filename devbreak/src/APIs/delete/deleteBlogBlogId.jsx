394dd8ee06d5ad869a1cb5d7621280746f5493c9
choigyulee <cgl1409@gmail.com>
2024-11-15 04:02:43 +0900

    feat<APIs>delete메소드파일작성

--- devbreak/src/APIs/delete/deleteBlogBlogId.jsx
+++ devbreak/src/APIs/delete/deleteBlogBlogId.jsx
@@ -1,1 +1,14 @@
 // 블로그 삭제 기능
+import axiosInstance from "../axiosInstance";
+
+export default async function deleteBlogBlogId() {
+  try {
+    const response = await axiosInstance.delete(
+      `/api/blog/${blogId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error('에러 발생:', error);
+    throw error;
+  }
+}
