a8deb339d4105b2e27f55edf143a846e4b117a6b
norangsuji <sujihong93@gmail.com>
2024-11-22 21:29:02 +0900

    refact<StartPage> StartPage 안쓰는 proptypes 삭제

--- devbreak/src/pages/StartPage.jsx
+++ devbreak/src/pages/StartPage.jsx
@@ -1,13 +1,10 @@
 import styled from "@emotion/styled";
 import CopywritingItem from "../components/StartPageItems/CopywritingItem";
 import NavBar from "../components/NavBar";
-import PropTypes from "prop-types";
 import { useRecoilValue } from "recoil";
-import { authState } from "../atoms/authAtoms"; 
-
+import { authState } from "../atoms/authAtoms";
 
 function StartPage() {
-
   const { isLoggedIn } = useRecoilValue(authState); // useAuth 훅을 사용하여 로그인 상태 가져오기
 
   return (
@@ -32,7 +29,6 @@ function StartPage() {
 // StartPage.propTypes = {
 //   isLoggedIn: PropTypes.bool.isRequired,
 // };
-
 
 export default StartPage;
 
