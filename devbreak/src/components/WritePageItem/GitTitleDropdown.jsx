7983f34d67b7ab50b57eb7f71a55204825549788
norangsuji <sujihong93@gmail.com>
2024-12-01 17:46:11 +0900

    fix <GitTitle> 토글 수정

--- devbreak/src/components/WritePageItem/GitTitleDropdown.jsx
+++ devbreak/src/components/WritePageItem/GitTitleDropdown.jsx
@@ -6,15 +6,12 @@ const GitTitleDropdown = ({ selectedValue, items, setSelectedValue, isOpen, togg
   const handleItemClick = (e, item) => {
     e.preventDefault();
     e.stopPropagation();
-    setSelectedValue(item); // 선택된 아이템 전체를 저장
+    setSelectedValue(item.title); // 선택된 아이템 전체를 저장
     toggleDropdown(); // 선택 후 드롭다운 닫기
   };
 
   const getDisplayValue = (value) => {
-    if (typeof value === "object" && value?.title) {
-      return value.title; // 객체일 경우 title을 반환
-    }
-    return value || "Select related issue or commit title"; // 문자열일 경우 그대로 반환
+    return value || "Select related issue or commit title"; // 단순히 title만 표시
   };
 
   return (
