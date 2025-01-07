d4d10b0e4c929319c6ab90013ceef64379f3d3b4
norangsuji <sujihong93@gmail.com>
2025-01-07 22:20:20 +0900

    design<Header>  flex direction 수정

--- devbreak/src/components/NotificationModal.jsx
+++ devbreak/src/components/NotificationModal.jsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import PropTypes from "prop-types";
 
 const NotificationModal = ({ notifications }) => {
-  const visibleNotifications = notifications.slice(0, 4);
+  const visibleNotifications = notifications.slice(0, 3);
 
   return (
     <ModalContainer>
