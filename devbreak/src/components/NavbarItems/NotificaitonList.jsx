7d05e783178876ccc1f1c1cd491f88fd44cee5ae
choigyulee <cgl1409@gmail.com>
2025-01-21 02:31:54 +0900

    fix id값 타 api 응답값으로 대체

--- devbreak/src/components/NavbarItems/NotificaitonList.jsx
+++ devbreak/src/components/NavbarItems/NotificaitonList.jsx
@@ -1,38 +1,62 @@
 import { useEffect, useState } from 'react';
 import getNotice from '../../APIs/get/getNotice';
+import getBlogBlogId from '../../APIs/get/getBlogBlogId';
+import getArticleArticleId from '../../APIs/get/getArticleArticleId';
 
-export default function NotificaitonList() {
+export default function NotificationList() {
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
     const fetchNotifications = async () => {
       try {
         const data = await getNotice();
-        const sortedData = data
-          .sort((a, b) => new Date(b.time) - new Date(a.time)) // 최신순으로 정렬
+        const sortedData = data.sort((a, b) => new Date(b.time) - new Date(a.time)); // 최신순으로 정렬
 
-        const formattedNotifications = sortedData.map((notice) => {
-          let message = '';
-          switch (notice.type) {
-            case "블로그 초대":
-              message = `${notice.instigator} invited you to join the blog '${notice.relatedId.blogId}'.`;
-              break;
-            case "블로그 즐겨찾기":
-              message = `${notice.instigator} added blog '${notice.relatedId.blogId}' to their favorites.`;
-              break;
-            case "글 좋아요":
-              message = `${notice.instigator} liked your article '${notice.relatedId.articleId}'.`;
-              break;
-            default:
-              message = `${notice.instigator} sent you a new notification.`;
-              break;
-          }
+        const formattedNotifications = await Promise.all(
+          sortedData.map(async (notice) => {
+            let message = '';
+            let blogName = '';
+            let articleTitle = '';
 
-          return {
-            ...notice,
-            message,
-          };
-        });
+            if (notice.type === "블로그 초대" || notice.type === "블로그 즐겨찾기") {
+              try {
+                const blogData = await getBlogBlogId(notice.relatedId.blogId);
+                blogName = blogData.blog_name;
+              } catch (error) {
+                console.error('Failed to fetch blog name:', error);
+              }
+            }
+
+            if (notice.type === "글 좋아요") {
+              try {
+                const articleData = await getArticleArticleId(notice.relatedId.articleId);
+                articleTitle = articleData.title;
+              } catch (error) {
+                console.error('Failed to fetch article title:', error);
+              }
+            }
+
+            switch (notice.type) {
+              case "블로그 초대":
+                message = `${notice.instigator} invited you to join the blog '${blogName}'.`;
+                break;
+              case "블로그 즐겨찾기":
+                message = `${notice.instigator} added blog '${blogName}' to their favorites.`;
+                break;
+              case "글 좋아요":
+                message = `${notice.instigator} liked your article '${articleTitle}'.`;
+                break;
+              default:
+                message = `${notice.instigator} sent you a new notification.`;
+                break;
+            }
+
+            return {
+              ...notice,
+              message,
+            };
+          })
+        );
 
         setNotifications(formattedNotifications);
       } catch (error) {
